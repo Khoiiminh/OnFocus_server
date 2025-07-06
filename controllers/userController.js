94f3cf5ecde36d60ed8b7ac027e52527777aa394
Khoiiminh <khoiminhnguyenhoang1804@gmail.com>
2025-07-06 16:54:44 +0700

    request for resetting password and reset new password

--- controllers/userController.js
+++ controllers/userController.js
@@ -2,10 +2,13 @@ import { PrismaClient } from '@prisma/client';
 const prisma = new PrismaClient();
 import bcrypt from 'bcrypt';
 import jwt from 'jsonwebtoken';
+import { randomBytes } from 'crypto';
+import { error } from 'console';
+import { localhost } from '../config/server.js';
 
 export async function register(req, res) {
     try {
-        const { name, email, password } = req.body;
+        const { name, email, password } = req.body;    // declare and take value from HTTP's request body
 
         // check if user already exists
         const existingUser = await prisma.users.findUnique({ where: { email } });
@@ -17,6 +20,9 @@ export async function register(req, res) {
         const hashedPassword = await bcrypt.hash(password, 10);     // 10 This value dictates 
                                                                     // the computational cost of hashing and, consequently, 
                                                                     // the level of security
+        // generate verification token
+        const verificationToken = randomBytes(32).toString('hex');  // This line of code generates a 64-character long, 
+                                                                    // highly random and secure hexadecimal string
 
         // create user
         const user = await prisma.users.create({
@@ -24,10 +30,20 @@ export async function register(req, res) {
                 name, 
                 email, 
                 password: hashedPassword, 
+                verification_token: verificationToken,
+                is_verified: false
             }
         });
 
-        res.status(201).json({user});
+        // simulate sending email
+        const verifyURL = `${localhost}/api/users/verify?token=${verificationToken}`;
+        console.log(`📩 Verify your email at: ${verifyURL}`);
+
+        res.status(201).json({
+            user, 
+            message: 'User rgistered! Please verify your email.',
+            verifyURL
+        });
     } catch (err) {
         res.status(500).json({ error: err.message});
     }
@@ -40,6 +56,10 @@ export async function login(req, res) {
         const user = await prisma.users.findUnique({ where: { email }});
         if ( !user ) {
             return res.status(401).json({ error: 'User not found' });
+        }
+
+        if ( !user.is_verified ) {
+            return res.status(403).json({error: 'Please, verify your email before logging in.'});
         }
 
         const match = await bcrypt.compare(password, user.password);
@@ -72,8 +92,100 @@ export async function getProfile(req, res) {
             return res.status(404).json({error: 'User not found' });
         }
 
-        res.json(user);
+        res.json({
+            user,   // payload
+            token: req.token
+        });
     } catch (err) {
         res.status(500).json({error: err.message});
     }
 }
+
+export async function verifyEmail(req, res) {
+    try {
+        const { token } = req.body;
+        if ( !token ) {
+            return res.status(400).json({error: 'Verification token missing'});
+        }
+
+        const user = await prisma.users.findUnique({where: {verification_token: token}});
+        if ( !user ) {
+            return res.status(400).json({error: 'Invalid or expired token'});
+        }
+
+        await prisma.users.update({
+            where: {id: user.id},
+            data: {
+                is_verified: true,
+                email_verified_at: new Date(),
+                verification_token: null
+            }
+        });
+
+        res.json({message: '✅ Email verified successfully!'});
+    } catch (err) {
+        res.status(500).json({error: err.message});
+    }
+};
+
+export async function requestPasswordReset(req, res) {
+    try {
+        const { email } = req.body;
+
+        const user = await prisma.users.findUnique({where: { email }});
+        if ( !user ) {
+            return res.status(404).json({ error: 'User not found'});
+        }
+
+        const resetToken = randomBytes(32).toString('hex');
+        const expires = new Date(Date.now() + 15 * 60 * 1000); // 15 mins 
+
+        await prisma.users.update({
+            where: {id: user.id},
+            data: {
+                verification_token: resetToken,
+                reset_token_expires: expires
+            },
+        });
+
+        const resetUrl = `${localhost}/api/users/reset-password?token=${resetToken}`;
+        console.log("🔐 Reset your password:", resetUrl);
+
+        res.json({message: 'Password reset link hs been sent (check logs for now).'});
+    } catch (err) {
+        res.status(500).json({error: err.message});
+    }
+};
+
+export async function resetPassword(req, res) {
+    try {
+        const { token } = req.query;   //declare and take value from key-value pair right in URL (after the '?' mark)
+        const { newPassword } = req.body;
+
+        const user = await prisma.users.findFirst({
+            where: {
+                verification_token: token,
+                reset_token_expires: { gt: new Date() },
+            },
+        });
+
+        if ( !user ) {
+            return res.status(400).json({error: 'Invalid or expired token'});
+        }
+
+        const hashedPassword = await bcrypt.hash(newPassword, 10);
+
+        await prisma.users.update({
+            where: {id: user.id},
+            data: {
+                password: hashedPassword,
+            verification_token: null,
+            reset_token_expires: null
+            },
+        });
+
+        res.json({message: '✅ Password updated successfully'});
+    } catch (err) {
+        res.status(500).json({error: err.message});
+    }
+};
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,17 +1,23 @@
 import { Router } from 'express';
-import { register, login, getProfile } from '../controllers/userController.js';import { authenticateToken } from '../middlewares/authMiddleware.js';
+import { register, login, getProfile, verifyEmail, requestPasswordReset, resetPassword } from '../controllers/userController.js';
+import { authenticateToken } from '../middlewares/authMiddleware.js';
 
 
 const router = Router();
 // register
 router.post('/register', register);
 
+// verify email
+router.get('/verify', verifyEmail);
+
+//reset passsword
+router.post('/request-password-reset', requestPasswordReset);
+router.post('/reset-password', resetPassword);
+
 // login
 router.post('/login', login);
 
 // protected routes
-router.get('/profile', authenticateToken, (req, res) => {
-    res.json({message: 'Protected Router', getProfile})
-});
+router.get('/profile', authenticateToken, getProfile);
 
 export default router;
